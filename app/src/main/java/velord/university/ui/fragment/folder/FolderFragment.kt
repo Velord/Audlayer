0a97743ea0413692c9ef52829406e155a77fd512
Velord <vladik.yaroshenko@yandex.ru>
2020-06-21 00:20:11 +0300

    Add: change AddToPlaylist UI

--- app/src/main/java/velord/university/ui/fragment/folder/FolderFragment.kt
+++ app/src/main/java/velord/university/ui/fragment/folder/FolderFragment.kt
@@ -435,7 +435,7 @@ class FolderFragment : ActionBarFragment(),
                             }
                         }
 
-                        actionImageButton.setupPopupMenuOnClick(
+                        actionImageButton.setupAndShowPopupMenuOnClick(
                             requireContext(),
                             initActionMenuStyle,
                             initActionMenuLayout,
